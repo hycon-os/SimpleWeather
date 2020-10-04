cba0ec3d6e717d53b54da32c1b6b4c064f7fcd87
Ethan Halsall <ethan.halsall@gmail.com>
2020-10-04 01:16:03 +0100

    Map icons in the same way as we did for omnijaws as openweather icons are a bit shit and limited
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- app/src/main/java/com/example/myapplication/FetchWeather.kt
+++ app/src/main/java/com/example/myapplication/FetchWeather.kt
@@ -46,12 +46,92 @@ class FetchWeather {
                 response: Response<JsonObject?>
             ) {
                 if (response.code() == 200) {
-                    val main = (response.body()?.getAsJsonObject("main"))
-                    var tmp = main?.get("temp")
-                    println(tmp)
+                    val main = response.body()?.getAsJsonObject("main")
+                    val sys = response.body()?.getAsJsonObject("sys")
+                    val weather = response.body()?.getAsJsonArray("weather")?.get(0)?.asJsonObject
+                    var sunset = sys?.get("sunset")!!.asLong
+                    var sunrise = sys?.get("sunrise").asLong
+                    var id = weather?.get("id")!!.asInt
+                    var temp = main?.get("temp")
+                    var icon = mapConditionIconToCode(id, sunrise, sunset)
+                    println(icon)
                 }
             }
         })
 
+    }
+
+    private fun mapConditionIconToCode(conditionId: Int, sunrise: Long, sunset: Long): Int {
+        val ut2 = System.currentTimeMillis() / 1000L
+        if (sunrise < ut2 && sunset > ut2) {
+            // First, use condition ID for specific cases
+            when (conditionId) {
+                202, 232, 211 -> return 4
+                212 -> return 3
+                221, 231, 201 -> return 38
+                230, 200, 210 -> return 37
+                300, 301, 302, 310, 311, 312, 313, 314, 321 -> return 9
+                500, 501, 520, 521, 531 -> return 11
+                502, 503, 504, 522 -> return 12
+                511 -> return 10
+                600, 620 -> return 14 // light snow
+                601, 621 -> return 16 // snow
+                602, 622 -> return 41 // heavy snow
+                611, 612 -> return 18 // sleet
+                615, 616 -> return 5 // rain and snow
+                741 -> return 20
+                711, 762 -> return 22
+                701, 721 -> return 21
+                731, 751, 761 -> return 19
+                771 -> return 23
+                781 -> return 0
+                800 -> return 32
+                801 -> return 34
+                802 -> return 28
+                803, 804 -> return 30
+                900 -> return 0 // tornado
+                901 -> return 1 // tropical storm
+                902 -> return 2 // hurricane
+                903 -> return 25 // cold
+                904 -> return 36 // hot
+                905 -> return 24 // windy
+                906 -> return 17 // hail
+            }
+        } else {
+            // First, use condition ID for specific cases
+            when (conditionId) {
+                202, 232, 211 -> return 4
+                212 -> return 3
+                221, 231, 201 -> return 47
+                230, 200, 210 -> return 45
+                300, 301, 302, 310, 311, 312, 313, 314, 321 -> return 9
+                500, 501, 520, 521, 531 -> return 11
+                502, 503, 504, 522 -> return 12
+                511 -> return 10
+                600, 620 -> return 14 // light snow
+                601, 621 -> return 16 // snow
+                602, 622 -> return 41 // heavy snow
+                611, 612 -> return 18 // sleet
+                615, 616 -> return 5 // rain and snow
+                741 -> return 20
+                711, 762 -> return 22
+                701, 721 -> return 21
+                731, 751, 761 -> return 19
+                771 -> return 23
+                781 -> return 0
+                800 -> return 31
+                801 -> return 33
+                802 -> return 27
+                803, 804 -> return 29
+                900 -> return 0 // tornado
+                901 -> return 1 // tropical storm
+                902 -> return 2 // hurricane
+                903 -> return 25 // cold
+                904 -> return 36 // hot
+                905 -> return 24 // windy
+                906 -> return 17 // hail
+            }
+        }
+        return -1
     }
 }
