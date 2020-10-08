73a6cda872325cde1c33fa52f4f5fe3ef814d9ab
Ethan Halsall <ethan.halsall@gmail.com>
2020-10-08 11:28:05 +0100

    Lets use a service to start work manager

--- app/src/main/java/com/revengeos/simpleweather/WeatherService.kt
+++ app/src/main/java/com/revengeos/simpleweather/WeatherService.kt
@@ -1,17 +1,22 @@
 package com.revengeos.simpleweather
 
-import android.content.BroadcastReceiver
-import android.content.Context
+import android.app.Service
 import android.content.Intent
+import android.os.IBinder
 import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
 import java.util.concurrent.TimeUnit
 
-class WorkManagerStartReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
+class WeatherService : Service() {
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val periodicWork =
             PeriodicWorkRequest.Builder(WeatherWorker::class.java, 15, TimeUnit.MINUTES)
                 .build()
         WorkManager.getInstance().enqueue(periodicWork)
+        return super.onStartCommand(intent, flags, startId)
     }
 }
